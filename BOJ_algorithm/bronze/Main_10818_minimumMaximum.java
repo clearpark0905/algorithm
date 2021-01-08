0008e37ce4fc3d4e667d1d20cb54dd67f8e6dda9
DESKTOP-0F73CKP\sanghyeok <psh2138@gmail.com>
2021-01-09 03:29:44 +0900

    BOJ_algorithm 문제풀이 추가

---
+++ BOJ_algorithm/bronze/Main_10818_minimumMaximum.java
@@ -0,0 +1,20 @@
+package BOJ_algorithm.bronze;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main_10818_minimumMaximum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        int[] a = new int[num];
+
+        for (int i = 0; i < num; i++) {
+            a[i] = sc.nextInt();
+        }
+
+        Arrays.sort(a);
+        System.out.println(a[0] + " " + a[a.length - 1]);
+        sc.close();
+    }
+}
---
+++ BOJ_algorithm/bronze/Main_2562_Maximum.java
@@ -0,0 +1,22 @@
+package BOJ_algorithm.bronze;
+
+import java.util.Scanner;
+
+public class Main_2562_Maximum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] num = new int[9];
+        int maxValue=0;
+        int maxValueIndex=0;
+        for (int i = 0; i < num.length; i++) {
+            num[i] = sc.nextInt();
+            if(num[i] > maxValue){
+                maxValue = num[i];
+                maxValueIndex = i+1;
+            }
+        }
+        System.out.println(maxValue);
+        System.out.println(maxValueIndex);
+        sc.close();
+    }
+}
