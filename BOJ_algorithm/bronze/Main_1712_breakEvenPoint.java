6722a27f0a39b6737740a871e4a6a3c60972b9d9
sanghyeok <psh2138@gmail.com>
2021-01-22 23:45:48 +0900

    algorithm 문제풀이 추가

--- BOJ_algorithm/bronze/Main_1712_breakEvenPoint.java
+++ BOJ_algorithm/bronze/Main_1712_breakEvenPoint.java
@@ -9,9 +9,9 @@ public class Main_1712_breakEvenPoint {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         String[] abc = br.readLine().split(" ");
-        long a = Long.parseLong(abc[0]);
-        long b = Long.parseLong(abc[1]);
-        long c = Long.parseLong(abc[2]);
+        long a = Long.parseLong(abc[0]); //고정비용
+        long b = Long.parseLong(abc[1]); //가변비용
+        long c = Long.parseLong(abc[2]); //가격
 
         if (c <= b) {
             bw.write(String.valueOf(-1));
