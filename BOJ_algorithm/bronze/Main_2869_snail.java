60b8d4fd20c9a098aacf66a2bff519cd23541078
sanghyeok <psh2138@gmail.com>
2021-01-22 23:58:45 +0900

    algorithm 문제풀이 추가

---
+++ BOJ_algorithm/bronze/Main_2869_snail.java
@@ -0,0 +1,24 @@
+package BOJ_algorithm.bronze;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main_2869_snail {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        double a = Integer.parseInt(st.nextToken());
+        double b = Integer.parseInt(st.nextToken());
+        double v = Integer.parseInt(st.nextToken());
+
+        double num = ((v-b)/(a-b))%1;
+        if ( num == 0 ) bw.write(String.valueOf((int) ((v-b)/(a-b))));
+        else bw.write(String.valueOf((int) ((v-b)/(a-b) + 1)));
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
